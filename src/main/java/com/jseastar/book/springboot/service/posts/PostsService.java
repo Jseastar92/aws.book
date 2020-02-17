3a217e206b7684cb5d413284fadb4b20fe910421
jSeastar <Jseastar92@gmail.com>
2020-02-17 23:19:54 +0900

    Posts Api, 서비스, Dto 작성, api 테스트 작성

---
+++ src/main/java/com/jseastar/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jseastar.book.springboot.service.posts;
+
+import com.jseastar.book.springboot.domain.posts.PostsRepository;
+import com.jseastar.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/jseastar/book/springboot/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.jseastar.book.springboot.web;
+
+import com.jseastar.book.springboot.service.posts.PostsService;
+import com.jseastar.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
