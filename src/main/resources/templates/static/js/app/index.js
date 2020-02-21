c74aeffe73f2dcb0cac6342db85fea8b8c707173
jSeastar <Jseastar92@gmail.com>
2020-02-21 22:45:14 +0900

    Chap 04. 머스테치 화면구성하기 - 전체화면 만들기

--- src/main/resources/templates/static/js/app/index.js
+++ src/main/resources/templates/static/js/app/index.js
@@ -1,13 +1,12 @@
 var =index {
-    init : function () {
+    init : function() {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
         });
     }
-,
 
-    save : function () {
+    save : function() {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
