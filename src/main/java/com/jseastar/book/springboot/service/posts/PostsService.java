0255c1897e7482961d10f44c4bef27e706c249de
jSeastar <Jseastar92@gmail.com>
2020-02-25 23:17:02 +0900

    Chap 4. 글 등록

--- src/main/java/com/jseastar/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jseastar/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jseastar.book.springboot.service.posts;
 
 import com.jseastar.book.springboot.domain.posts.Posts;
 import com.jseastar.book.springboot.domain.posts.PostsRepository;
+import com.jseastar.book.springboot.web.dto.PostsListResponseDto;
 import com.jseastar.book.springboot.web.dto.PostsResponseDto;
 import com.jseastar.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jseastar.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,7 +26,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -37,4 +41,10 @@ public class PostsService {
 
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+            return postsRepository.findAllDesc().stream()
+                    .map(PostsListResponseDto::new)
+                    .collect(Collectors.toList());
+    }
 }
