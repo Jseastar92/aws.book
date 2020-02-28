039125f6ccec562628c8ed4908cc9e6ccfcb1925
jSeastar <Jseastar92@gmail.com>
2020-02-28 22:06:40 +0900

    Chap 05. 구글 로그인 연동하기 - Spring Security & Oauth2 SeesionUser 작성

--- src/main/java/com/jseastar/book/springboot/config/auth/CustomOauth2UserService.java
+++ src/main/java/com/jseastar/book/springboot/config/auth/CustomOauth2UserService.java
@@ -1,6 +1,7 @@
 package com.jseastar.book.springboot.config.auth;
 
 import com.jseastar.book.springboot.config.auth.dto.OAuthAttributes;
+import com.jseastar.book.springboot.config.auth.dto.SessionUser;
 import com.jseastar.book.springboot.domain.user.User;
 import com.jseastar.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
