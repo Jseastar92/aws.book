9c6ae0f212247a0be6b9bd4f95b9fdd60b7d98ed
jSeastar <Jseastar92@gmail.com>
2020-03-03 23:59:39 +0900

    Chap 05. 구글 로그인 연동하기 - 네이버 로그인 !

--- src/main/java/com/jseastar/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jseastar/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -30,7 +30,6 @@ public class OAuthAttributes {
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
         if("naver".equals(registrationId)){
-            System.out.println("registrationId = " + registrationId);
             return ofNaver("id", attributes);
         }
         return ofGoogle(userNameAttributeName, attributes);
@@ -51,12 +50,13 @@ public class OAuthAttributes {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("profileImage"))
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
+
     }
 
     public User toEntity(){
